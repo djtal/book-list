ce88ab38d01f1f59a0c0a1443bfae1af70a5592a
Guillaume Garcera <guillaume.garcera@gmail.com>
2015-05-18 22:53:49 +0200

    Add nested route to show each book

---
+++ app/routes/books/book.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function(params){
+    var books = this.modelFor('books');
+    return(books.findBy('id', params.id));
+  }
+});
