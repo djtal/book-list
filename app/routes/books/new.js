4099136add1a21b494d13bc25bad331e47127ac5
Guillaume Garcera <guillaume.garcera@gmail.com>
2015-05-19 22:20:01 +0200

    Handle saving new book

--- app/routes/books/new.js
+++ app/routes/books/new.js
@@ -1,7 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
-    return(this.store.createRecord('book'));
+  actions: {
+    createBook: function() {
+      var controller = this.get('controller');
+      var title = controller.get('title');
+      var description = controller.get('description');
+      var book = this.get('store').createRecord('book', { title: title, description, description });
+      book.save().then(function(book) {
+        controller.set('title', '');
+        controller.set('description', '');
+        controller.transitionTo('books.book', book)
+      })
+    }
   }
 });
